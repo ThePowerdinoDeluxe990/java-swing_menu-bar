f66091a644130cb8ddea093ec0437f0cc0dc6825
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-05-12 11:52:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+  class Main{
+    public static void main(String[] ar) {
+        Formulario formulario1=new Formulario();
+        formulario1.setBounds(10,20,300,200);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }    
+}
